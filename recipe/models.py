6292aa83428ac5e31ee4720d83d08f6ac09464b0
sam-dev-1729 <sam.dev.1729@gmail.com>
2023-12-17 14:45:06 +0330

    first commit

--- recipe/models.py
+++ recipe/models.py
@@ -1,11 +1,11 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 # Create your models here.
 
+
 class Recipe(models.Model):
-    
-    name = models.CharField(max_length = 150)
+    name = models.CharField(max_length=150)
     ingrediants = models.TextField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
